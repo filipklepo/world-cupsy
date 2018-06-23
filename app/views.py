f9da578f2b0a9ac1a5811bba572739b5a3f5fb9a
Filip Klepo <filipklepo@gmail.com>
2018-06-23 13:58:43 +0200

    Add basic views rendering. Update settings.py.

--- app/views.py
+++ app/views.py
@@ -1,4 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='login')
 def index(request):
-    return HttpResponse("Hello world!")
+    return render(request, 'index.html')
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+
+@login_required(login_url='login')
+def logout(request):
+    auth_logout(request)
+    return redirect('index')
+
+@login_required(login_url='login')
+def vote(request):
+    return HttpResponse("Vote here!")
--- world_cupsy/settings.py
+++ world_cupsy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wu+2cd=3&53g6akwgfw(2hu$l6!mjhv8at6l1%tebikecnbwg='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -114,8 +114,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
